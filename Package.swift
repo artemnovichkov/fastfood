df72bc47d0b9675b35ec8431c94fd2a86f8ba929
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-19 14:21:48 +0600

    Add FastfoodCore

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "fastfood",
     dependencies: [
         .package(url: "https://github.com/johnsundell/files.git", from: "1.0.0"),
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "FastfoodCore",
+            dependencies: ["Files"],
+            path: "./Sources/fastfood-core"),
         .target(
             name: "fastfood",
-            dependencies: ["Files"]),
+            dependencies: ["FastfoodCore"]),
     ]
 )
