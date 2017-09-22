ab61981854c94a67e4b297c5fcd7fe076a29e273
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-22 08:52:21 +0600

    Add updating of fastfile

--- Sources/fastfood-core/Fastfood.swift
+++ Sources/fastfood-core/Fastfood.swift
@@ -28,23 +28,37 @@ public final class Fastfood {
     }
     
     public func run() throws {
-        arguments.remove(at: 0)
-        guard arguments.count == 1 else {
-            throw Error.missingArguments
-        }
-        let path = arguments[0]
-        let tag = try currentTag() ?? "1.0"
-        let fastfoodFolder = try fileSystem.createFolderIfNeeded(at: Keys.fastfoodPath)
-        
-        if !fastfoodFolder.containsFile(named: Keys.fastfile) {
-            print("Start downloading from \(path)...")
-            let data = try load(path: path, tag: tag)
-            print("Downloading has finished")
-            try save(data: data, withName: "fastfile-test-\(tag)/", to: fastfoodFolder)
-        }
-        try updateFastfileIfNeeded(withImport: "import \(Keys.fastfoodPath)/" + Keys.fastfile, tag: tag)
-        try cleanup(fastfoodFolder)
-        print("🚀 Done!")
+//        arguments.remove(at: 0)
+//        guard arguments.count == 1 else {
+//            throw Error.missingArguments
+//        }
+//        let path = arguments[0]
+//        let tag = try currentTag() ?? "1.0"
+//        let fastfoodFolder = try fileSystem.createFolderIfNeeded(at: Keys.fastfoodPath)
+//
+//        if !fastfoodFolder.containsFile(named: Keys.fastfile) {
+//            print("Start downloading from \(path)...")
+//            let data = try load(path: path, tag: tag)
+//            print("Downloading has finished")
+//            try save(data: data, withName: "fastfile-test-\(tag)/", to: fastfoodFolder)
+//        }
+//        try updateFastfileIfNeeded(withImport: "import \(Keys.fastfoodPath)/" + Keys.fastfile, tag: tag)
+//        try cleanup(fastfoodFolder)
+//        print("🚀 Done!")
+        try updateLocalFastfile()
+    }
+    
+    func updateLocalFastfile() throws {
+        let fastfoodPath = "/usr/local/bin/.fastfood"
+        let tempPath = fastfoodPath + "/tmp"
+        try? Folder(path: tempPath).delete()
+        clone(fromPath: "https://github.com/artemnovichkov/fastfile-test.git",
+              toLocalPath: tempPath)
+        let fastfoodFolder = try Folder(path: fastfoodPath)
+        let fastfile = try File(path: tempPath + "/Fastfile")
+        try? fastfoodFolder.file(named: "Fastfile").delete()
+        try fastfile.move(to: fastfoodFolder)
+        try? Folder(path: tempPath).delete()
     }
     
     private func load(path: String, tag: String) throws -> Data {
@@ -63,6 +77,14 @@ public final class Fastfood {
         let process = Process()
         process.launchPath = "/usr/bin/env"
         process.arguments = ["unzip", input, "-d", output]
+        process.launch()
+        process.waitUntilExit()
+    }
+    
+    private func clone(fromPath path: String, toLocalPath localPath: String) {
+        let process = Process()
+        process.launchPath = "/usr/bin/env"
+        process.arguments = ["git", "clone", path, localPath]
         process.launch()
         process.waitUntilExit()
     }
