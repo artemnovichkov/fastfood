27b7dd16ea5f7eb389f7002f80cd3d24c6e65f95
Artem Novichkov <artem.novichkov@rosberry.com>
2017-09-28 16:21:30 +0600

    Move Swiftlint to postbuild action

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+swiftlint
 swift test --parallel
--- buddybuild_postclone.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-swift package generate-xcodeproj
-
-if ! which swiftlint >/dev/null; then
-brew install swiftlint
-fi
-
-swiftlint
