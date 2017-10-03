48dc1074bf85f1a30db11736728630371ca94b70
Artem Novichkov <artem.novichkov@rosberry.com>
2017-10-03 10:24:31 +0600

    Update argument tests for version

--- Tests/FastfoodCoreTests/ArgumentsSpec.swift
+++ Tests/FastfoodCoreTests/ArgumentsSpec.swift
@@ -32,16 +32,10 @@ final class ArgumentsSpec: XCTestCase {
         XCTAssertEqual(arguments?.url, URL(string: urlString))
     }
 
-    func testShortTagOptionParsing() {
-        let tagString = "1.0"
-        let arguments = Arguments(arguments: ["-t", tagString])
-        XCTAssertEqual(arguments?.tag, tagString)
-    }
-
-    func testShortBranchOptionParsing() {
-        let branchString = "develop"
-        let arguments = Arguments(arguments: ["-b", branchString])
-        XCTAssertEqual(arguments?.branch, branchString)
+    func testShortVersionOptionParsing() {
+        let versionString = "1.0"
+        let arguments = Arguments(arguments: ["-v", versionString])
+        XCTAssertEqual(arguments?.version, versionString)
     }
 
     func testShortForceOptionParsing() {
@@ -57,16 +51,10 @@ final class ArgumentsSpec: XCTestCase {
         XCTAssertEqual(arguments?.url, URL(string: urlString))
     }
 
-    func testLongTagOptionParsing() {
-        let tagString = "1.0"
-        let arguments = Arguments(arguments: ["--tag", tagString])
-        XCTAssertEqual(arguments?.tag, tagString)
-    }
-
-    func testLongBranchOptionParsing() {
-        let branchString = "develop"
-        let arguments = Arguments(arguments: ["--branch", branchString])
-        XCTAssertEqual(arguments?.branch, branchString)
+    func testLongVersionOptionParsing() {
+        let versionString = "1.0"
+        let arguments = Arguments(arguments: ["--version", versionString])
+        XCTAssertEqual(arguments?.version, versionString)
     }
 
     func testLongForceOptionParsing() {
@@ -81,13 +69,8 @@ final class ArgumentsSpec: XCTestCase {
         XCTAssertNil(arguments)
     }
 
-    func testWrongTagOption() {
-        let arguments = Arguments(arguments: ["--tag"])
-        XCTAssertNil(arguments)
-    }
-
-    func testWrongBranchOption() {
-        let arguments = Arguments(arguments: ["--branch"])
+    func testWrongVersionOption() {
+        let arguments = Arguments(arguments: ["--version"])
         XCTAssertNil(arguments)
     }
 }
